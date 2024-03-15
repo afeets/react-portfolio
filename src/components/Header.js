8c1abba3463ba046c4c1c5c44be46e5e9270496c
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-15 11:43:17 +0700

    Create-React-App boilerplate. Setup Header, Link components

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import './Header.css';
+import Link from './Link';
+
+function Header({ logo }){
+  return (
+    <header className='App-header'>
+      <img src={logo} className='App-logo' alt='logo' />
+      <p>Edit <code>src/App.js</code></p>
+      <Link 
+        url='https://reactjs.org'
+        title='Learn React'
+      />
+    </header>
+  )
+}
+
+export default Header;
